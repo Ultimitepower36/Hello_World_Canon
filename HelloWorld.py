a815e4699158917a933f589c4f938552d8050289
Canon <canon.heise@gmail.com>
2023-04-20 13:41:50 -0600

    Quality of life update
    A slight alteration to the code to make the printed result more readable.

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,7 @@ Part1 = "Hello World!"
 Part2 = "My name is Canon."
 Part3 = "How are you?"
 
-
+print(f'\n');
 print(f'{Part1} {Part2} {Part3}\n');
 print(f'{Part2} {Part3} {Part1}\n');
 print(f'{Part3} {Part1} {Part2}\n');
